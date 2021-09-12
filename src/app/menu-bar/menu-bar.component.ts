d6b7183c724b1d4b43f8173d190b46c3a5971f18
ptrswr <pswirkaitis@wp.pl>
2021-09-12 10:52:24 +0200

    finished menubar

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-menu-bar',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent implements OnInit {
+  
+  items: MenuItem[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        icon: 'pi pi-bookmark',
+        routerLink: '/'
+      },
+      {
+          label: 'List of books',
+          icon: 'pi pi-book',
+          routerLink: '/'
+      },
+      {
+          label: 'Import from google',
+          icon: 'pi pi-google',
+          routerLink: '/import'
+      },
+      {
+        label: 'Search for books',
+        icon: 'pi pi-search',
+        routerLink: '/search'
+
+    }
+  ];
   }
 
 }
